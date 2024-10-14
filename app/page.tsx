64caeb3003af10a3d861ecf346d960f9fa791d26
seonghoon07 <chltjdgns1009@gmail.com>
2024-10-15 01:44:51 +0900

    DELETE: gitkeep 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 import { redirect } from "next/navigation";
-import { connectDB } from "@/utils/db/database";
 
-const Home = async () => {
-  const client = await connectDB();
-  const db = client?.db("musicany");
-  const result = await db?.collection("user_cred").find().toArray();
-  console.log(result);
+const Home = () => {
+  redirect("/main");
 };
 
 export default Home;
