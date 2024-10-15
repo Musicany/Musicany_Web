14977ce341456bafdaf4d47fc09cc52e8f4469a9
seonghoon07 <chltjdgns1009@gmail.com>
2024-10-16 01:49:27 +0900

    CHORE: 코드정리

--- app/(workspace)/composition/ifCode/page.tsx
+++ app/(workspace)/composition/ifCode/page.tsx
@@ -1,19 +1,27 @@
 "use client";
 
 import React from "react";
+import styled from "styled-components";
+import { flex } from "@/styles";
 
 import CompositionModal from "@/components/modals/compositionModal/page";
-import * as S from "./style";
+
+const Layout = styled.div`
+  background-color: #353535;
+  width: 100%;
+  height: 100vh;
+  ${flex.CENTER};
+`;
 
 const IfCode = () => {
   return (
-    <S.Layout>
+    <Layout>
       <CompositionModal
         type="choose"
         title="이미 생각한 코드진행이 있으신가요?"
         subTitle="코드가 이미 있으시다면 ‘예' 를 클릭해주세요"
       />
-    </S.Layout>
+    </Layout>
   );
 };
 
--- app/(workspace)/composition/ifCode/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import { flex, font, theme } from "@/styles";
-
-export const Layout = styled.div`
-  background-color: #353535;
-  width: 100%;
-  height: 100vh;
-  ${flex.CENTER};
-`;
