51fd28864a7eb179feaea5dc6fda70e69b1e5626
seonghoon07 <chltjdgns1009@gmail.com>
2024-10-17 07:23:48 +0900

    ADD&DELETE: customAxios 설정 및 next auth 삭제

--- app/components/@common/Header/page.tsx
+++ app/components/@common/Header/page.tsx
@@ -3,24 +3,11 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Logo from "@/assets/logo";
-import { signIn, signOut, useSession } from "next-auth/react";
 import * as S from "./style";
 
 const Header = () => {
   const router = useRouter();
-  const { data: session } = useSession();
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
-
-  const handleWorkspaceClick = (
-    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
-  ) => {
-    event.preventDefault();
-    if (!session) {
-      alert("로그인이 필요한 서비스입니다.");
-    } else {
-      router.push("/workspace");
-    }
-  };
 
   return (
     <S.Layout>
@@ -34,7 +21,6 @@ const Header = () => {
           onMouseEnter={() => setHoveredIndex(1)}
           onMouseLeave={() => setHoveredIndex(null)}
           href="workspace"
-          onClick={handleWorkspaceClick}
         >
           <S.NavText>WorkSpace</S.NavText>
         </S.NavItem>
@@ -49,40 +35,25 @@ const Header = () => {
       </S.MainNav>
 
       <S.LoginNav>
-        {session ? (
-          <S.NavItem
-            $isHovered={hoveredIndex !== null && hoveredIndex !== 3}
-            onMouseEnter={() => setHoveredIndex(3)}
-            onMouseLeave={() => setHoveredIndex(null)}
-            onClick={() => signOut()}
-            href="#"
-          >
-            <S.NavText>{session.user?.name}님</S.NavText>
-          </S.NavItem>
-        ) : (
-          <>
-            <S.NavItem
-              $isHovered={hoveredIndex !== null && hoveredIndex !== 3}
-              onMouseEnter={() => setHoveredIndex(3)}
-              onMouseLeave={() => setHoveredIndex(null)}
-              onClick={(event) => {
-                event.preventDefault();
-                signIn();
-              }}
-              href=""
-            >
-              <S.NavText>Sign In</S.NavText>
-            </S.NavItem>
-            <S.NavItem
-              $isHovered={hoveredIndex !== null && hoveredIndex !== 4}
-              onMouseEnter={() => setHoveredIndex(4)}
-              onMouseLeave={() => setHoveredIndex(null)}
-              href="/register"
-            >
-              <S.NavText>Sign Up</S.NavText>
-            </S.NavItem>
-          </>
-        )}
+        <S.NavItem
+          $isHovered={hoveredIndex !== null && hoveredIndex !== 3}
+          onMouseEnter={() => setHoveredIndex(3)}
+          onMouseLeave={() => setHoveredIndex(null)}
+          onClick={(event) => {
+            event.preventDefault();
+          }}
+          href=""
+        >
+          <S.NavText>Sign In</S.NavText>
+        </S.NavItem>
+        <S.NavItem
+          $isHovered={hoveredIndex !== null && hoveredIndex !== 4}
+          onMouseEnter={() => setHoveredIndex(4)}
+          onMouseLeave={() => setHoveredIndex(null)}
+          href="/register"
+        >
+          <S.NavText>Sign Up</S.NavText>
+        </S.NavItem>
       </S.LoginNav>
     </S.Layout>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
 import StyledComponentsRegistry from "@/lib/styledComponentsRegistry";
-// eslint-disable-next-line import/no-named-as-default
-import AuthContext from "../context/AuthContext";
 
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => (
   <html lang="en">
     <body>
-      <StyledComponentsRegistry>
-        <AuthContext>{children}</AuthContext>
-      </StyledComponentsRegistry>
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
     </body>
   </html>
 );
