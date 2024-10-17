1334ab2ac33ac5c83fe32f2c58d85c3d872c9f06
seonghoon07 <chltjdgns1009@gmail.com>
2024-10-17 10:38:44 +0900

    CHORE: username localstorage에서 받아옴

--- app/(workspace)/workspace/page.tsx
+++ app/(workspace)/workspace/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import AddButton from "@/components/buttons/addButton";
 import { useRouter } from "next/navigation";
@@ -8,11 +8,19 @@ import * as S from "./style";
 
 const Workspace = () => {
   const router = useRouter();
+  const [userName, setUsername] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedUsername = localStorage.getItem("username");
+    if (storedUsername) {
+      setUsername(storedUsername);
+    }
+  }, []);
   return (
     <S.Layout>
       <S.SideLayout>
         <S.NameLayout>
-          <S.UserName>최성훈</S.UserName>
+          <S.UserName>{userName}</S.UserName>
         </S.NameLayout>
         <S.StepLayout>
           <S.Step>Composing</S.Step>
@@ -29,14 +37,6 @@ const Workspace = () => {
             </AddButton>
           </S.ContentHeader>
           <S.Works>
-            <S.Work>
-              <S.Title>제목입니다제목입니다</S.Title>
-              <S.Date>2024.10.09</S.Date>
-            </S.Work>
-            <S.Work>
-              <S.Title>제목입니다제목입니다</S.Title>
-              <S.Date>2024.10.09</S.Date>
-            </S.Work>
             <S.Work>
               <S.Title>제목입니다제목입니다</S.Title>
               <S.Date>2024.10.09</S.Date>
